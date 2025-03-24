d6bc539fe46b01995eb5f7debed20a330bc2936b
marselo-4 <jordan08022006@alumnes.ilerna.com>
2025-03-24 16:59:02 +0100

    Inventario
    he arreglado unos errores que habia ya que no funcionaba correctamente el codigo

--- src/Javones_y_mazmorras/Inventario.java
+++ src/Javones_y_mazmorras/Inventario.java
@@ -7,22 +7,22 @@ public class Inventario {
 
     public Inventario() {
         this.equipamiento = new ArrayList<>();
+    }
 
-    public void agregarEquipamiento (Equipamiento equipamiento) {
+    public void agregarEquipamiento(Equipamiento equipamiento) {
         this.equipamiento.add(equipamiento);
-        System.out.println(equipamiento.getNombre() + "se ha añadido al inventario");
+        System.out.println(equipamiento.getNombre() + " se ha añadido al inventario");
 
     }
 
     public void mostrarInventario() {
-        if (equipamiento.estaVacio());
-        System.out.println("el inventario esta vacio");
-    } else {
-        System.out.println("Inventario");
-
-        for (int i = 0; i < equipamiento.espacio(); i++) {
-            Equipamiento e = equipamiento.get(i);
-            System.out.println("- " + e.getNombre() + " (" + e.getTipo() + ")");
+        if (equipamiento.isEmpty()) {
+            System.out.println("El inventario está vacío");
+        } else {
+            System.out.println("Inventario:");
+            for (int i = 0; i < equipamiento.size(); i++) {
+                Equipamiento e = equipamiento.get(i);
+                System.out.println("- " + e.getNombre() + " (" + e.getTipo() + ")");
         }
     }
     
